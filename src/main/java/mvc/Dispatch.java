953adb986e93078f0489b1bf4a13d42f0192c87b
Karol Ziabski <karolziabski8@gmail.com>
2018-11-12 14:47:37 +0100

    Initial commit

---
+++ src/main/java/mvc/Dispatch.java
@@ -0,0 +1,21 @@
+package mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletException;
+
+public class Dispatch extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
