fc29154e6a48605d7c185a80d4160106fbc81ec1
Karol Ziabski <karolziabski8@gmail.com>
2018-11-26 22:59:23 +0100

    next modifies

--- src/main/java/mvc/HomeController.java
+++ src/main/java/mvc/HomeController.java
@@ -1,13 +1,13 @@
 package mvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(){
         return "home";
     }
--- web/WEB-INF/view/home.jsp
+++ web/WEB-INF/view/home.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
   User: karol
-  Date: 2018-11-12
-  Time: 14:40
+  Date: 2018-11-26
+  Time: 22:58
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
