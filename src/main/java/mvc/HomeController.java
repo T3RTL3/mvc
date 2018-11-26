9384ec0aa94867119512777410d6bc15c51b7edc
Karol Ziabski <karolziabski8@gmail.com>
2018-11-26 22:38:33 +0100

    Configure

--- src/main/java/mvc/HomeController.java
+++ src/main/java/mvc/HomeController.java
@@ -1,12 +1,13 @@
 package mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @GetMapping("/")
     public String home(){
         return "home";
     }
