e8fe21bfd7bf17c6cab8f22543f038e03b4f80f5
Karol Ziabski <karolziabski8@gmail.com>
2018-11-27 21:12:25 +0100

    java code configuration

---
+++ src/main/java/mvc/Config.java
@@ -0,0 +1,21 @@
+package mvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "mvc")
+public class Config extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/jsp/", ".jsp");
+
+    }
+}
--- src/main/java/mvc/HomeController.java
+++ src/main/java/mvc/HomeController.java
@@ -12,4 +12,9 @@ public class HomeController {
     public String home(){
         return "home";
     }
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "showForm";
+
+    }
 }
